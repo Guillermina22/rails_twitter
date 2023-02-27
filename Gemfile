1f2d52012a4b7a0187e2a1062fef1759c4f094b2
Guillermina Lanza <guillermina@gogrow.dev>
2023-02-27 17:56:35 +0000

    install-rubocop

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.0'
@@ -7,14 +9,14 @@ gem 'rails', '~> 6.1.7', '>= 6.1.7.2'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails', '~> 6.2.0'
   gem 'faker', '~>3.1.1'
-  gem "rspec-rails"
+  gem 'rspec-rails'
+  gem 'rubocop-rails', require: false
 end
 
 group :development do
--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class WidgetsController < ApplicationController
-  before_action :set_widget, only: [:show, :update, :destroy]
+  before_action :set_widget, only: %i[show update destroy]
 
   # GET /widgets
   def index
@@ -39,13 +41,14 @@ class WidgetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_widget
-      @widget = Widget.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def widget_params
-      params.require(:widget).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_widget
+    @widget = Widget.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def widget_params
+    params.require(:widget).permit(:name)
+  end
 end
